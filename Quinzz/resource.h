97c21ea3b7a356cde613733d533fb165f2739b01
Alina-sul <alina.sulkovsky@gmail.com>
2021-09-28 10:08:23 +0300

    Added new dialog for new test

--- Quinzz/resource.h
+++ Quinzz/resource.h
@@ -7,6 +7,8 @@
 #define IDS_ABOUTBOX                    101
 #define IDD_QUINZZ_DIALOG               102
 #define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     130
+#define IDD_NEW_TEST                    130
 #define IDC_BUTTON1                     1000
 #define IDC_NEW_TEST                    1000
 
@@ -14,7 +16,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_RESOURCE_VALUE        132
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
