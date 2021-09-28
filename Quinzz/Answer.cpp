b61ba43c7d7b1931c32d90d1239d7d1eeb3b5b25
Alina-sul <alina.sulkovsky@gmail.com>
2021-09-28 10:51:24 +0300

    Answer.cpp

--- Quinzz/Answer.cpp
+++ Quinzz/Answer.cpp
@@ -1,2 +1,29 @@
 #include "pch.h"
 #include "Answer.h"
+
+Answer::Answer(bool c, string a)
+{
+	setFlag(c);
+	setAnswer(a);
+}
+
+void Answer::setFlag(bool c) 
+{
+	this->correct = c;
+}
+
+void Answer::setAnswer(string s) 
+{
+	this->answer = s;
+
+}
+
+bool Answer::getFlag() const
+{
+	return this->correct;
+}
+
+string Answer::getAnswer() const
+{
+	return this->answer;
+}
