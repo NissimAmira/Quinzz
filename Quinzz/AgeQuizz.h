7c555100fc5538b1bc467688a646228f815a235a
Alina-sul <alina.sulkovsky@gmail.com>
2021-09-30 02:49:44 +0300

    Age - functions created

--- Quinzz/AgeQuizz.h
+++ Quinzz/AgeQuizz.h
@@ -4,7 +4,17 @@ class AgeQuizz :
     public DataCollectionQuizz
 {
 private:
-    int age[];
+    int *age_count;
 public:
+    AgeQuizz() : DataCollectionQuizz(), age_count(nullptr) {};
+    AgeQuizz(CString t, int num_of_q);
+    ~AgeQuizz() { delete[] this->age_count; };
+
+    void setAgeCount(int* arr);
+    void updateAgeCount(int age);
+    int* getAgeCount();
+
+    Question ageCollectionQuestion();
+
 };
 
