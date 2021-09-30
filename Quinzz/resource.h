ce815b842919567912a01467714513f096aa7437
Alina-sul <alina.sulkovsky@gmail.com>
2021-09-30 08:32:25 +0300

    Edited QE and NQ

--- Quinzz/resource.h
+++ Quinzz/resource.h
@@ -23,6 +23,7 @@
 #define IDC_EXIST_BTN                   1000
 #define IDC_CREATE_QUIZ                 1000
 #define IDC_CREATE_QUIZ_BTN             1000
+#define IDC_NEXT                        1000
 #define IDC_NEW_QUIZ_BTN2               1001
 #define IDC_NEW_QUIZ                    1001
 #define IDC_BIO_L1                      1002
@@ -62,6 +63,24 @@
 #define IDC_NUM_QUESTIONS               1059
 #define IDC_SIMPLE_QUIZ_RADIO           1060
 #define IDC_DCQUIZ_RADIO                1061
+#define IDC_COMBO1                      1064
+#define IDC_STATIC_Q_INDEX              1066
+#define IDC_PREVIOUS                    1067
+#define IDC_Q_TEXT                      1068
+#define IDC_STATIC_Q_TEXT               1069
+#define IDC_STATIC_A_1                  1070
+#define IDC_TEXT_A1                     1071
+#define IDC_CHECK_A1                    1072
+#define IDC_STATIC_A_2                  1073
+#define IDC_STATIC_NAME                 1073
+#define IDC_TEXT_A3                     1074
+#define IDC_CHECK_A2                    1075
+#define IDC_STATIC_A_3                  1076
+#define IDC_TEST_A2                     1077
+#define IDC_CHECK_A3                    1078
+#define IDC_TEXT_A4                     1079
+#define IDC_STATIC_A_4                  1080
+#define IDC_CHECK_A4                    1081
 
 // Next default values for new objects
 // 
@@ -69,7 +88,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        147
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1064
+#define _APS_NEXT_CONTROL_VALUE         1074
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
